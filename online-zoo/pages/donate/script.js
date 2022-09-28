2c95b5176a897f388547672e0d9b923d6b265f4e
Artemiy Gudz <mercartem@mail.ru>
2022-09-28 18:22:42 +0300

    feat: add burger menu

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -37,3 +37,18 @@ function getDotActive() {
     }
 }
 window.addEventListener('resize', getDotActive)
+
+// Burger меню
+
+const btnBurger = document.querySelector('.header__burger');
+const burger = document.querySelector('.header__nav');
+const overlay = document.querySelector('.overlay')
+
+btnBurger.addEventListener('click', () => {
+    burger.classList.add('header__nav_active');
+    overlay.classList.add('overlay_active');
+})
+overlay.addEventListener('click', () => {
+    burger.classList.remove('header__nav_active');
+    overlay.classList.remove('overlay_active');
+})
